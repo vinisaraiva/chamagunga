a6a12c9560523eaea45e396db47ca19741627d89
Vinicius <vinisaraiva@gmail.com>
2023-08-28 10:16:18 -0300

    Update 1_🏠_Home.py

--- 1_🏠_Home.py
+++ 1_🏠_Home.py
@@ -14,6 +14,14 @@ st.set_page_config(
     layout="wide"
 )
 
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
 data = pd.read_csv("riochamagunga.csv", encoding="ISO-8859-1", sep=';')
 print(data.head())
 
