82ca1ffc7d2f27dc621408491064cb9fbaecc7dc
Vinicius <vinisaraiva@gmail.com>
2023-10-21 10:56:35 -0300

    Update 2_📊_Analise.py

--- pages/2_📊_Analise.py
+++ pages/2_📊_Analise.py
@@ -18,7 +18,17 @@ st.set_page_config(
     page_icon="📑",
     layout="wide"
 )
-
+st.markdown("""
+    <style>
+        .reportview-container {
+            margin-top: -2em;
+        }
+        #MainMenu {visibility: hidden;}
+        .stDeployButton {display:none;}
+        footer {visibility: hidden;}
+        #stDecoration {display:none;}
+    </style>
+""", unsafe_allow_html=True)
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 st.markdown("""
